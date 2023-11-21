82c9d2a5d723f1e477a16e79d31cb92c634bec65
Marc Durdin <marc@durdin.net>
2023-11-21 14:28:24 +1000

    chore: update fonts

--- fonts.js
+++ fonts.js
@@ -2,6 +2,7 @@ function selectFont(id) {
   ta1.classList.remove('font-khmer-busra-test');
   ta1.classList.remove('font-khmer-mondulkiri');
   ta1.classList.remove('font-noto-sans-khmer');
+  ta1.classList.remove('font-khmer-os-battambang');
   ta1.classList.remove('font-system');
   ta1.classList.add('font-'+id);
 }
@@ -10,6 +11,7 @@ function addFontsToMenu() {
   menuDropdown.add('#font-khmer-busra-test', 'Use Khmer Busra Test font (new structure)');
   menuDropdown.add('#font-khmer-mondulkiri', 'Use Khmer Mondulkiri font (old structure)');
   menuDropdown.add('#font-noto-sans-khmer', 'Use Noto Sans Khmer font (old structure)');
+  menuDropdown.add('#font-khmer-os-battambang', 'Use Khmer OS Battambang font (old structure)');
   menuDropdown.add('#font-system', 'Use system font (old structure)');
   menuDropdown.set('#font-khmer-busra-test');
 }
